95dd4e36adfa2322c53c9fbd984d53c3f147275e
elemenpetir <rafiletsdoit@gmail.com>
2026-06-01 18:51:08 +0700

    setup react frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Internal IT Asset Management</h1>
+      <p>React frontend setup is working</p>
+    </div>
+  )
+}
+
+export default App

1ffe6ddbde04e866775c21eb879d0d51199e6354
elemenpetir <rafiletsdoit@gmail.com>
2026-06-07 21:38:41 +0700

    enable cors for frontend development

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const app = express()
-const authRoutes = require('./routes/authRoutes');
-const assetCategoryRoutes = require('./routes/assetCategoryRoutes')
-const assetRoutes = require('./routes/assetRoutes')
-const assetAssignmentRoutes = require('./routes/assetAssignmentRoutes')
-const auditLogRoutes = require('./routes/auditLogRoutes')
-const errorMiddleware = require('./middleware/errorMiddleware')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const assetCategoryRoutes = require("./routes/assetCategoryRoutes");
+const assetRoutes = require("./routes/assetRoutes");
+const assetAssignmentRoutes = require("./routes/assetAssignmentRoutes");
+const auditLogRoutes = require("./routes/auditLogRoutes");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  }),
+);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/asset-categories', assetCategoryRoutes)
-app.use('/api/assets', assetRoutes)
-app.use('/api/asset-assignments', assetAssignmentRoutes)
-app.use('/api/audit-logs', auditLogRoutes)
-app.use(errorMiddleware)
+app.use(express.json());
 
-module.exports = app
+app.use("/api/auth", authRoutes);
+app.use("/api/asset-categories", assetCategoryRoutes);
+app.use("/api/assets", assetRoutes);
+app.use("/api/asset-assignments", assetAssignmentRoutes);
+app.use("/api/audit-logs", auditLogRoutes);
+app.use(errorMiddleware);
+
+module.exports = app;

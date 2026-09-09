0e0ba880deaf525bb0d95465c7a048c7d3cd0b3e
elemenpetir <rafiletsdoit@gmail.com>
2026-09-09 17:44:27 +0700

    fix(frontend): richColors toasts for semantic feedback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ import Departments from "./pages/departments/Departments";
 function App() {
   return (
     <>
-      <Toaster position="top-center" />
+      <Toaster position="top-center" richColors />
       <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/activate-account" element={<ActivateAccount />} />
